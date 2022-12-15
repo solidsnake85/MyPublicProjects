d9306009afc893efa20c5e2d9b67688ab91762fa
solidsnake85 <matt.meyer@mac.com>
2022-12-14 17:29:25 -0800

    Create find_common_numbers_in_sets.py

---
+++ find_common_numbers_in_sets.py
@@ -0,0 +1,11 @@
+# Find the common numbers of the 2 sets
+
+set_1 = {10, 15, 18, 19, 22, 45, 56, 79, 82, 80, 81, 99, 100, 56}
+
+set_2 = {5, 6, 8, 18, 23, 15, 80, 19, 72, 33, 82, 83}
+
+print(set_1 & set_2)
+
+# or
+# answer = set_1.intersection(set_2)
+# print(answer)
